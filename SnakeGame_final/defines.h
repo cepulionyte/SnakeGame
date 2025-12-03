f9ddf9579201b3d52a76ad1209cf424e7e5db183
ievac <ievac@LAPTOP-E74HTA96>
2025-12-03 22:05:03 +0200

    pakeiciau pradini gyvates greiti

--- SnakeGame_final/defines.h
+++ SnakeGame_final/defines.h
@@ -6,6 +6,6 @@ const int CELL_SIZE = 20;
 const int GRID_WIDTH = WINDOW_WIDTH / CELL_SIZE;
 const int GRID_HEIGHT = WINDOW_HEIGHT / CELL_SIZE;
 
-const float INITIAL_SPEED = 8.0f;
+const float INITIAL_SPEED = 7.0f;
 const float SPEED_INCREMENT = 0.3f;
 const int INITIAL_SNAKE_LENGTH = 3;
